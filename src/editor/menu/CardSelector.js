5be807c640160a49c2e11ae01b89439a89360ec3
Nidhi <nidhi@pro.to>
2018-12-13 18:53:14 +0530

    - removed feault cards

--- src/editor/menu/CardSelector.js
+++ src/editor/menu/CardSelector.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import MenuItem from './MenuItem'
-import DropDown from '../util/DropDown'
-import Card from '../../components/Card/Card'
-import axios from 'axios'
-import './CardSelector.css'
-import { ReactComponent as ProtoIcon } from '../../assets/icons/proto-icon.svg'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import MenuItem from "./MenuItem";
+import DropDown from "../util/DropDown";
+import Card from "../../components/Card/Card";
+import axios from "axios";
+import "./CardSelector.css";
+import { ReactComponent as ProtoIcon } from "../../assets/icons/proto-icon.svg";
 // window.axios = axios
 
 const propTypes = {
@@ -14,144 +14,132 @@ const propTypes = {
   children: PropTypes.object,
   command: PropTypes.func,
   isActive: PropTypes.bool,
-  isAllowed: PropTypes.bool
-}
+  isAllowed: PropTypes.bool,
+};
 
 class CardSelector extends Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
 
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleChange = this.handleChange.bind(this)
-    this.toggleSelector = this.toggleSelector.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.toggleSelector = this.toggleSelector.bind(this);
 
-    this.state = { isOpened: false}
+    this.state = { isOpened: false };
   }
 
   hideModal = () => {
-    this.setState({ isOpened: false })
-  }
+    this.setState({ isOpened: false });
+  };
 
-  handleSubmit (e) {
-    e.stopPropagation()
-    e.preventDefault()
-    this.toggleSelector()
+  handleSubmit(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    this.toggleSelector();
     if (this.state.card) {
-      let card = this.state.card
+      let card = this.state.card;
       this.props.run({
         url: card.url,
         caption: card.title,
-        'data-card-id': card.key,
-        'data-template-id': card['data-template-id']
-      })
+        "data-card-id": card.key,
+        "data-template-id": card["data-template-id"],
+      });
     }
   }
 
-  handleChange (e) {
-    let card = this.cardList.find(x => x.key === e)
+  handleChange(e) {
+    let card = this.cardList.find(x => x.key === e);
 
     this.setState({
-      card: card
-    })
+      card: card,
+    });
   }
 
-  toggleSelector (e) {
-    let isOpened = this.state.isOpened
-    this.setState({ isOpened: !isOpened, card: null })
+  toggleSelector(e) {
+    let isOpened = this.state.isOpened;
+    this.setState({ isOpened: !isOpened, card: null });
   }
 
-  render () {
+  render() {
     let buttonProps = {
       faIcon: this.props.faIcon,
       selection: this.props.selection,
       run: this.toggleSelector,
       isActive: this.props.isActive,
-      isAllowed: this.props.isAllowed
-    }
+      isAllowed: this.props.isAllowed,
+    };
 
-    this.cardList = []
+    let cards_request = this.props.cards_request;
+    this.cardList = [];
     if (cards_request) {
       axios
         .get(cards_request.url, {
-          headers: { 'Access-Token': cards_request.token }
+          headers: { "Access-Token": cards_request.token },
         })
-        .then(function (response) {
+        .then(function(response) {
           this.cardList = response.data.map(x => {
             return {
               url: x.iframe_url,
               title: x.name,
               key: x.id,
-              'data-template-id': x.template_card_id
-            }
-          })
-        })
-    } else {
-      this.cardList = [
-        {
-          url:
-            'https://cdn.protograph.pykih.com/99e448b6fcb668c5a3d4/index.html?view_cast_id=7a312bd07ab133968703ec4e&base_url=https://www.responsiblebiz.org',
-          size: 75,
-          height: 419,
-          align: 'center',
-          caption: 'Image',
-          'data-card-id': 62499,
-          'data-template-id': 47
-        }
-      ]
-
-      this.cardList.map(e => {
-        e.key = e['data-card-id']
-        e.title = e.caption
-        return e
-      })
+              "data-template-id": x.template_card_id,
+            };
+          });
+        });
     }
 
     return (
-      <div className='card-selector-button'>
+      <div className="card-selector-button">
         <Modal isOpened={this.state.isOpened} handleClose={this.hideModal}>
-        <h3 className="modal-heading">Insert Card</h3>
+          <h3 className="modal-heading">Insert Card</h3>
           <DropDown
             options={this.cardList}
             onChange={this.handleChange}
             placeHolder="Select Card"
             width="100px"
           />
-          <div className='card-preview'>
+          <div className="card-preview">
             {this.state.card && (
               <Card attrs={this.state.card} isSelected={false} />
             )}
           </div>
 
-          <div className="proto-button" disabled={!this.state.card} onClick={this.handleSubmit}>
+          <div
+            className="proto-button"
+            disabled={!this.state.card}
+            onClick={this.handleSubmit}
+          >
             Submit
           </div>
         </Modal>
         <MenuItem {...buttonProps}>
-          <ProtoIcon style={{ height: '20px', width: '20px' }} />
+          <ProtoIcon style={{ height: "20px", width: "20px" }} />
         </MenuItem>
       </div>
-    )
+    );
   }
 }
 
 const Modal = ({ handleClose, isOpened, children }) => {
   const showHideClassName = isOpened
-    ? 'modal display-block'
-    : 'modal display-none'
+    ? "modal display-block"
+    : "modal display-none";
 
   return (
     <div className={showHideClassName}>
-      <section className='modal-main'>
+      <section className="modal-main">
         {children}
-        <div className="proto-button-close" onClick={handleClose}>Close</div>
+        <div className="proto-button-close" onClick={handleClose}>
+          Close
+        </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-CardSelector.propTypes = propTypes
+CardSelector.propTypes = propTypes;
 // CardSelector.defaultProps = defaultProps;
-export default CardSelector
+export default CardSelector;
 
 // {this.state && this.state.isOpened && (
 //           <div
